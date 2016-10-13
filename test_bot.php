c512c156202279c638dde9a287a955cc87298b9d
Kanokwan <kanokwan.ta@inet.co.th>
2016-10-13 15:36:07 +0700

    edit eat=>test_bot

--- test_bot.php
+++ test_bot.php
@@ -12,7 +12,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			//$text = $event['message']['text'];
+			$text = "ยัง ทำให้กินหน่อย"
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
@@ -21,7 +22,7 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
-																											// Make a POST Request to Messaging API to reply to sender
+			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
@@ -30,7 +31,8 @@ if (!is_null($events['events'])) {
 
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-																											$ch = curl_init($url);
+			
+			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
